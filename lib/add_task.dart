cb647041b80ec53c19a330c59123f8e2c2cff492
Hui Lee <lihui870920@gmail.com>
2019-05-10 14:18:13 +0800

    添加任务

--- lib/add_task.dart
+++ lib/add_task.dart
@@ -14,6 +14,7 @@ class _AddTaskState extends State<AddTask> {
 
   @override
   Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(title: Text(widget._title)),
       body: new Padding(
@@ -22,39 +23,70 @@ class _AddTaskState extends State<AddTask> {
           key: formKey,
           child: new Column(
             children: <Widget>[
-              DropdownButton<String>(
-                isExpanded: true,
-                value: dropdownValue,
-                onChanged: (String newValue) {
-                  setState(() {
-                    dropdownValue = newValue;
-                  });
-                },
-                items: <String>['One', 'Two', 'Free', 'Four']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-              TextField(
-                maxLines: null,
-                keyboardType: TextInputType.multiline,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: '备注',
+              Container(
+                child: DropdownButton<String>(
+                  isExpanded: true,
+                  value: dropdownValue,
+                  onChanged: (String newValue) {
+                    setState(() {
+                      dropdownValue = newValue;
+                    });
+                  },
+                  items: <String>['One', 'Two', 'Free', 'Four']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
                 ),
               ),
-              MaterialButton(
-                onPressed: () {},
-                textColor: Colors.white,
-                color: Colors.red,
-                child: new Text(
-                  '保存',
-                  style: TextStyle(fontSize: 20.0),
+              Container(
+                margin: const EdgeInsets.only(top: 8.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.calendar_today),
+                    labelText: '开始时间',
+                  ),
+                  keyboardType: TextInputType.datetime,
                 ),
               ),
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.calendar_today),
+                    labelText: '结束时间',
+                  ),
+                  keyboardType: TextInputType.datetime,
+                  
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 16.0),
+                child: TextField(
+                  maxLines: 2,
+                  keyboardType: TextInputType.multiline,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: '备注',
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 16.0),
+                width: screenSize.width,
+                height: 46.0,
+                child: MaterialButton(
+                  onPressed: () {},
+                  textColor: Colors.white,
+                  color: Colors.red,
+                  child: new Text(
+                    '保存',
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                ),
+              )
             ],
           ),
         ),
